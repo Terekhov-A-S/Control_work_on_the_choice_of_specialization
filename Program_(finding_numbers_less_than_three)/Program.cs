9a3d99cff9a4ace3ef2fe1a6cd2d2351acfc55d5
KoT <Kotto@student.su>
2022-09-18 17:40:23 +0400

    final commit for branch

--- Program_(finding_numbers_less_than_three)/Program.cs
+++ Program_(finding_numbers_less_than_three)/Program.cs
@@ -3,41 +3,41 @@
 либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами */
 Console.Clear();
-void PrintArray(string [] array)          // Метод печатает массив
+void PrintArray(string[] array)
 {
-    Console.Write("[");
+    Console.Write("[ ");
     for (int i = 0; i < array.GetLength(0); i++)
     {
-       Console.Write(array[i]  + " ");
+        Console.Write(array[i] + " ");
     }
-    Console.Write("]");
+    Console.WriteLine("]");
 }
-
-Console.WriteLine("Программа находит строки, введенные пользователем, в которых не более трех символов");
+Console.WriteLine("_-=* ПРОГРАММА НАХОДИТ СТРОКИ, ВВЕДЕННЫЕ ПОЛЬЗОВАТЕЛЕМ, В КОТОРЫХ НЕ БОЛЕЕ ТРЕХ СИМВОЛОВ *=-_");
 Console.Write("Задайте желаемое количество строк: ");
-int length = Convert.ToInt32(Console.ReadLine());
-string[] array = new string [length];
-Console.WriteLine("Введите значение строки: ");
-int counter = 0;
-
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[arrayLength];
+int count = 0;
 for (int i = 0; i < array.GetLength(0); i++)
 {
+    Console.Write($"Введите значение строки {i + 1}: ");
     array[i] = Convert.ToString(Console.ReadLine());
-    if(array[i].Length <= 3) counter++;
+    if (array[i].Length <= 3) count++;
 }
-string[] secondArray = new string [counter];
-counter = 0;
+string[] filteredArray = new string[count];
+count = 0;
 for (int i = 0; i < array.GetLength(0); i++)
 {
     if (array[i].Length <= 3)
     {
-        secondArray[counter] = array[i];
-        counter++;
+        filteredArray[count] = array[i];
+        count++;
     }
 }
+Console.WriteLine("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+Console.WriteLine();
+Console.Write("Массив с Вашими значениями: ");
 PrintArray(array);
-Console.Write(" - ");
-PrintArray(secondArray);
-
-
-
+Console.Write("Массив с отобранными значениями: ");
+PrintArray(filteredArray);
+Console.WriteLine();
+Console.WriteLine("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
